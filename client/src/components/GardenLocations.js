8f3deeb5d07f1bf46fb7088990f52dbad76d733f
drew23c <andrewcaldwell@ac.c4q.nyc>
2018-12-11 15:02:53 -0500

    working on css

--- client/src/components/GardenLocations.js
+++ client/src/components/GardenLocations.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './styles/gardensLocations.css';
 
-const AllLocations = ({location:{id, boro, garden_name, address, latitude, longitude, neighborhoodname, postcode, cross_streets}, isFilter}) =>{
+const AllLocations = ({location:{id, boro, garden_name, address, latitude, longitude, neighborhoodname, postcode, cross_streets}}) =>{
     return(
         <div>
-            <div className="gardens">
-                <div className="gardens-locations">
+            <ul className="gardens">
+                <li className="gardens-locations">
                     <a href={`https://www.google.com/maps/place/${address}/@${latitude},${longitude},17z?hl=en&authuser=0`} target="_blank" key={id}>
-                        <h3>{garden_name}</h3>
-                        <h4>{boro}</h4>
-                        <p>{neighborhoodname}</p>
-                        <p>{cross_streets}</p>
-                        <p>{address}</p>
-                        <p>{postcode}</p>
+                    <h3>{garden_name}</h3>
+                    <h4>{boro}</h4>
+                    <p>{neighborhoodname}</p>
+                    <p>{cross_streets}</p>
+                    <p>{address}</p>
+                    <p>{postcode}</p>
                     </a>
-                </div>
-            </div>
+                </li>
+            </ul>
         </div>
     )
 }
